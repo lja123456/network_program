9a3423a933b762cb9b5c75af529f958f1ea3eb02
lja <867734398@qq.com>
2021-05-09 23:43:59 +0800

    second

---
+++ ftp_tcp/tcp_Server.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+
+
+int main(int argc, char *argv[])
+{
+        int fd_socket;  
+
+        //1. 创建套接子
+
+
+        //2. 填充结构体 sockaddr_in
+
+
+        //3. 绑定套接子
+
+
+        //4. 监听套接子
+
+
+        //5. 等待连接 accept
+
+
+        //6. 收发数据
+
+}
